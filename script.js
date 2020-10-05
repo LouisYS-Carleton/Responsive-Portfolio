61ca113f34a2fd620714b7c3a67b07a5a09dd46e
Louis Yacksmith <louisyscarleton@gmail.com>
2020-10-05 15:07:48 -0400

    Updated portfolio, pretty much updated everything.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
